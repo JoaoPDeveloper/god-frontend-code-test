bc647653ff22f7bcc30950dfccbe8256b487d3af
JoaoPDeveloper <joao_entreprise@hotmail.com>
2023-09-27 23:42:33 -0300

    feat: home components and layout adjust

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,57 +1,51 @@
 import Image from "next/image";
-import { Card, CardContent, Flex, Link, Row, Spacer, Text } from "vcc-ui";
-import styles from "../../public/css/CarCard.module.css";
+import { Flex, Link, Spacer, Text } from "vcc-ui";
 import { Car } from "../types/car.interface";
+
+import styles from "../../public/css/components/carCard.module.css";
 
 interface CardProps {
   car: Car;
 }
+
 export function CarCard({ car }: CardProps) {
   return (
     <div className={styles.cardWrapper}>
-      <Card href={"/learn/" + car?.id}>
-        <CardContent>
-          <Text variant="bates">{car?.bodyType}</Text>
-          <Flex
-            extend={{
-              justifyContent: "flex-start",
-              alignItems: "center",
-              padding: 10,
-              flexDirection: "row",
-            }}
-          >
-            <Text variant="amundsen">{car?.modelName}</Text>
-            <Text variant="bates" subStyle={"inline-link"}>
-              {car?.modelType}
-            </Text>
-          </Flex>
-          <Spacer />
-          <Image
-            src={car.imageUrl}
-            alt={car.modelName}
-            width="250"
-            height="250"
-          />
-          <Spacer />
-          <Flex
-            extend={{
-              border: "1px solid grey",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "row",
-            }}
-          >
-            <Row>
-              <Link href="https://www.volvocars.com/" arrow="right">
-                SHOP
-              </Link>
-              <Link href="https://www.volvocars.com/" arrow="right">
-                LEARN
-              </Link>
-            </Row>
-          </Flex>
-        </CardContent>
-      </Card>
+      <Text variant="bates" subStyle="emphasis">
+        {car?.bodyType}
+      </Text>
+      <Flex
+        extend={{
+          justifyContent: "flex-start",
+          alignItems: "center",
+          flexDirection: "row",
+          margin: 0,
+        }}
+      >
+        <Text variant="amundsen" extend={{ margin: 0, padding: 0 }}>
+          {car?.modelName}
+        </Text>
+        <Text variant="bates" subStyle="inline-link" extend={{ margin: 0 }}>
+          {car?.modelType}
+        </Text>
+      </Flex>
+      <Spacer />
+      <Image src={car.imageUrl} alt={car.modelName} width="250" height="200" />
+      <Spacer />
+      <Flex
+        extend={{
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "row",
+        }}
+      >
+        <Link href="https://www.volvocars.com/" arrow="right">
+          SHOP
+        </Link>
+        <Link href="https://www.volvocars.com/" arrow="right">
+          LEARN
+        </Link>
+      </Flex>
     </div>
   );
 }
