d5fad60afaaaf4b11333614a959e01ad3c692b17
JoaoPDeveloper <joao_entreprise@hotmail.com>
2023-09-27 23:03:15 -0300

    feat: create card component

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,4 +1,6 @@
-import { Card, CardContent, Flex, Row, Spacer, Text } from "vcc-ui";
+import Image from "next/image";
+import { Card, CardContent, Flex, Link, Row, Spacer, Text } from "vcc-ui";
+import styles from "../../public/css/CarCard.module.css";
 import { Car } from "../types/car.interface";
 
 interface CardProps {
@@ -6,18 +8,50 @@ interface CardProps {
 }
 export function CarCard({ car }: CardProps) {
   return (
-    <Card href={"/learn/" + car?.id}>
-      <CardContent>
-        <Text variant="bates">{car?.bodyType}</Text>
-        <Flex>
-          <Row>
-        <Text variant="amundsen">{car?.modelName}</Text>
-        <Text variant="bates" subStyle={"inline-link"}>{car?.modelType}</Text>
-          </Row>
-        </Flex>
-        <Spacer />
-        <Text> This is a is a link that will take you to volvocars.com</Text>
-      </CardContent>
-    </Card>
+    <div className={styles.cardWrapper}>
+      <Card href={"/learn/" + car?.id}>
+        <CardContent>
+          <Text variant="bates">{car?.bodyType}</Text>
+          <Flex
+            extend={{
+              justifyContent: "flex-start",
+              alignItems: "center",
+              padding: 10,
+              flexDirection: "row",
+            }}
+          >
+            <Text variant="amundsen">{car?.modelName}</Text>
+            <Text variant="bates" subStyle={"inline-link"}>
+              {car?.modelType}
+            </Text>
+          </Flex>
+          <Spacer />
+          <Image
+            src={car.imageUrl}
+            alt={car.modelName}
+            width="250"
+            height="250"
+          />
+          <Spacer />
+          <Flex
+            extend={{
+              border: "1px solid grey",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "row",
+            }}
+          >
+            <Row>
+              <Link href="https://www.volvocars.com/" arrow="right">
+                SHOP
+              </Link>
+              <Link href="https://www.volvocars.com/" arrow="right">
+                LEARN
+              </Link>
+            </Row>
+          </Flex>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
